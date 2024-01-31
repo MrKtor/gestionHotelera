ec6f98e64a3c51a45a07c776dd95e56fed509c80
Héctor Rivas <hectorrivasexposito@gmail.com>
2024-01-31 11:14:21 +0100

    commit lo q sea

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const session = require('express-session');
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 const methodOverride = require('method-override');
 var dateFilter = require('nunjucks-date-filter');
-// var env = new nunjucks.Environment();
 dotenv.config();
 
 
-const Habitacion = require(__dirname + "/routes/habitaciones");
-const Limpieza = require(__dirname + "/routes/limpiezas");
-// const auth = require(__dirname + '/routes/auth');
+const habitacion = require(__dirname + "/routes/habitaciones");
+const limpieza = require(__dirname + "/routes/limpiezas");
+const auth = require(__dirname + '/routes/auth');
 
 mongoose.connect(process.env.DATABASE_URL); 
 
@@ -26,11 +26,15 @@ env.addFilter('date', dateFilter);
 
 app.set('view engine', 'njk');
 
-// env.addFilter('date', dateFilter);
-// dateFilter.setDefaultFormat('YYYY');
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+// Configuración de la sesión
+app.use(session({
+    secret: '1234',
+    resave: true,
+    saveUninitialized: false,
+    expires: new Date(Date.now() + (30 * 60 * 1000))
+  }));
 // Middleware para procesar otras peticiones que no sean GET o POST
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -41,13 +45,17 @@ app.use(methodOverride(function (req, res) {
 }));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/public', express.static(__dirname + '/public'));
-// app.use('/auth', auth);
-app.use("/habitaciones", Habitacion);
-app.use("/limpiezas", Limpieza);
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+  });
+app.use("/auth", auth);
+app.use("/habitaciones", habitacion);
+app.use("/limpiezas", limpieza);
 
-// app.get('/', (req, res) => {
-//     res.redirect('/login');
-//   });
+app.get('/', (req, res) => {
+    res.redirect('/auth/login');
+  });
 
 
 app.listen(process.env.PORT);
--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,28 @@
-// const express = require("express");
+const express = require("express");
+const Usuario = require("../models/usuario");
+const router = express.Router();
 
-// const Usuario = require(__dirname + "/models/usuario");
+router.get("/login", (req, res) => {
+    res.render("login");
+});
 
-// const router = express.Router();
+router.post("/login", (req, res) => {
+    Usuario.findOne({
+        login: req.body.login,
+        password: req.body.password,
+    }).then((resultado) => {
+        if (resultado) {
+            req.session.usuario = resultado;
+            res.redirect("/habitaciones");
+        } else {
+            res.render("login", { error: "Usuario o contraseña incorrectos" });
+        }
+    });
+});
 
+router.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/auth/login");
+});
 
-
-// router.get("/login", (req, res) => {
-//     res.render("login");
-// });
-
-// router.post("/login", (req, res) => {
-//     Usuario.findOne({
-//         login: req.body.login,
-//         password: req.body.password,
-//     }).then((resultado) => {
-//         if (resultado) {
-//             req.session.usuario = resultado.login;
-//             res.redirect("/habitaciones");
-//         } else {
-//             res.render("login", { error: "Usuario o contraseña incorrectos" });
-//         }
-//     });
-// });
-
-// router.get("/logout", (req, res) => {
-//     req.session.destroy();
-//     res.redirect("/");
-// });
-
-// module.exports = router;
+module.exports = router;
