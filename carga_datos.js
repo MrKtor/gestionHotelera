a6de3e1e59b8dd790ffffd4c27d4d352502f0997
Héctor Rivas <hectorrivasexposito@gmail.com>
2024-01-31 19:50:05 +0100

    pfff

--- carga_datos.js
+++ carga_datos.js
@@ -1,26 +1,40 @@
 const mongoose = require("mongoose");
 const Usuario = require(__dirname + "/models/usuario");
 
-mongoose.connect('mongodb://127.0.0.1:27017/hotel', { useNewUrlParser: true, useUnifiedTopology: true });
+async function cargarDatos() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/hotel');
 
-//USUARIOS CARGADOS PARA EL EJERCICIO
-let usuarios = [
-    new Usuario({
+    let usuarios = [
+      new Usuario({
         login: "usuario5",
         password: "1234567"
-    }),
-    new Usuario({
+      }),
+      new Usuario({
         login: "usuario2",
         password: "123456789"
-    }),
-    new Usuario({
+      }),
+      new Usuario({
         login: "usuario3",
         password: "123456789"
-    }),
-    new Usuario({
+      }),
+      new Usuario({
         login: "usuario4",
         password: "123456789"
-    })
-];
+      })
+    ];
 
-usuarios.forEach(u => u.save());
+    // Utilizar Promise.all para esperar a que todas las operaciones de guardado se completen
+    await Promise.all(usuarios.map(u => u.save()));
+
+    console.log('Datos cargados con éxito.');
+  } catch (error) {
+    console.error('Error al cargar datos:', error);
+  } finally {
+    // Cerrar la conexión después de cargar datos
+    await mongoose.disconnect();
+  }
+}
+
+// Llamar a la función para cargar datos
+cargarDatos();
